b528599c89d87b2d088146e10427ab3dd1320580
Kaushik1998 <ksncoc@gmail.com>
2020-04-23 14:46:04 +0530

    do while MenuDrivenGame

---
+++ corejava/Assignments/src/com/techlab/game/MenuGame.java
@@ -0,0 +1,65 @@
+package com.techlab.game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class PlayGame2 {
+	void game() {
+		Random r = new Random();
+		Scanner sc = new Scanner(System.in);
+		int random = r.nextInt(10);
+		int choice = 99;
+		System.out.println("Enter a number between 0 - 9");
+		while (true) {
+			choice = sc.nextInt();
+			if (choice == random) {
+				System.out.println("You have guessed correct");
+				break;
+			} else if (choice > random) {
+				System.out.println("Try lower");
+				continue;
+			}
+
+			else if (choice < random) {
+				System.out.println("Try Higher");
+				continue;
+			}
+		}
+		sc.close();
+	}
+}
+
+public class MenuGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		boolean flag = true;
+		System.out.println(" 1. Start Game \n 2. Exit");
+		int ch = s.nextInt();
+		PlayGame pg = new PlayGame2();
+		if (ch == 1) {
+			do {
+				pg.game();
+				System.out.println(" 1. Restart the game \n 2. Exit");
+				ch = s.nextInt();
+				switch (ch) {
+				case 1:
+					System.out.println("Game again");
+					break;
+				case 2:
+					flag = false;
+					System.out.println("Game exitting");
+					break;
+				default:
+					System.out.println("Wrong choice");
+					break;
+				}
+			} while (flag == true);
+		} else {
+			System.out.println("Game closed");
+		}
+		s.close();
+	}
+
+}
